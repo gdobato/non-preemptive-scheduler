1af4be9407fab45eb8df569c6b6af197bd2ba29f
Gabriel <dobatog@gmail.com>
2022-10-11 00:39:50 +0200

    Add simple led blinky

--- Readme.md
+++ Readme.md
@@ -3,16 +3,21 @@
 Small project based on [cortex-m-quickstart template](https://github.com/rust-embedded/cortex-m-quickstart) to run Rust on a Cortex-M4F target (STM32F429I-DISC1 board)
 
 ## Installation (Unix-like OS)
-- Toolchain
+**Toolchain**
 ```
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 ```
-- Suport for ARM-CM targets
+**Suport for ARM-CM targets**
 ```
 rustup target add thumbv6m-none-eabi thumbv7m-none-eabi thumbv7em-none-eabi thumbv7em-none-eabihf
 ```
 
 ## Build
+**Unoptimized**
 ```
 cargo build
+```
+**Optimized**
+```
+cargo build --release
 ```
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,42 @@
 #![no_main]
 
 use cortex_m::{asm};
-use cortex_m_rt::{entry};
+use cortex_m_rt::{entry, exception};
 use panic_halt as _;
+use stm32f4xx_hal as hal;
+use hal::{prelude::*, pac};
+
 
 #[entry]
 fn main() -> ! {
-    loop {
-        asm::nop();
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut systick = cp.SYST;
+    let rcc = dp.RCC.constrain();
+
+    let clks = rcc
+        .cfgr  // Max values considering HSE source (8 MHz)
+        .use_hse(8.MHz())
+        .hclk(180.MHz())
+        .sysclk(180.MHz())
+        .pclk1(45.MHz())
+        .pclk2(90.MHz())
+        .freeze();
+    
+    systick.enable_interrupt();  // Debug purposes
+    let mut delay = systick.delay(&clks);
+
+    let gpio_g = dp.GPIOG.split();
+    let mut led_red = gpio_g.pg14.into_push_pull_output();
+
+    loop {        
+        led_red.toggle();
+        delay.delay_ms(250_u8);
     }
+}
+
+#[exception]
+fn SysTick() {
+    asm::nop()
 }
