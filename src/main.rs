2ab06f93c8079fc6a7e7134e1760573e22171096
Gabriel <dobatog@gmail.com>
2022-10-12 00:58:41 +0200

    Add RTT support

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,14 @@ use cortex_m::{asm};
 use cortex_m_rt::{entry, exception};
 use panic_halt as _;
 use stm32f4xx_hal as hal;
-use hal::{prelude::*, pac};
+use hal::{pac, prelude::*};
+use rtt_target::{rtt_init_print as log_init, rprintln as log};
 
 #[entry]
 fn main() -> ! {
+    log_init!();
+    log!("Init Target...");
+
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
 
@@ -23,14 +27,16 @@ fn main() -> ! {
         .pclk1(45.MHz())
         .pclk2(90.MHz())
         .freeze();
-    
-    systick.enable_interrupt();  // Debug purposes
-    let mut delay = systick.delay(&clks);
 
+    systick.enable_interrupt();
+    let mut delay = systick.delay(&clks);
+    
     let gpio_g = dp.GPIOG.split();
     let mut led_red = gpio_g.pg14.into_push_pull_output();
-
-    loop {        
+    
+    loop {
+        #[cfg(debug_assertions)]
+        log!("Toggling led...");
         led_red.toggle();
         delay.delay_ms(250_u8);
     }
@@ -38,5 +44,5 @@ fn main() -> ! {
 
 #[exception]
 fn SysTick() {
-    asm::nop()
+    asm::nop();
 }
